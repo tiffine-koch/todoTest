93073a00dde1677708ce03c2d05152b96659b692
Apple <tiffine.koch@gmail.com>
2016-02-15 09:49:46 -0800

    setting up index.html + home.html

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', require('./routes/index');
-app.use('/users', require('./routes/users');
+app.use('/', require('./routes/index'));
+// app.use('/users', require('./routes/users'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
